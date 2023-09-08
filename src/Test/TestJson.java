12f36a4017a8a7c041c838a931c29530455b2573
brandonT2002 <brandonteja02@gmail.com>
2023-09-08 00:09:39 -0600

    Generación de gráfica de pie, a partir de datos almacenados en hashmap

--- src/Test/TestJson.java
+++ src/Test/TestJson.java
@@ -10,6 +10,7 @@ import javax.swing.UnsupportedLookAndFeelException;
 import com.formdev.flatlaf.intellijthemes.FlatOneDarkIJTheme;
 import Env.Env;
 import Graphs.BarGraph;
+import Graphs.PieChart;
 
 public class TestJson {
     public static void main(String[] args) {
@@ -20,14 +21,15 @@ public class TestJson {
         Env table = new Env();
         table.addFile("File1.json");
         table.addAttributes("File1.json", "Titulo", "Grafica de Barras");
+        table.addAttributes("File1.json", "Titulo1", "Grafica de Pie");
         table.addAttributes("File1.json", "Eje X", "Deportes");
         table.addAttributes("File1.json", "Eje Y", "Integrantes");
-        table.addAttributes("File1.json", "Valor1", 400);
-        table.addAttributes("File1.json", "Valor2", 20);
-        table.addAttributes("File1.json", "Valor3", 100);
+        table.addAttributes("File1.json", "Valor1", 14);
+        table.addAttributes("File1.json", "Valor2", 5);
+        table.addAttributes("File1.json", "Valor3", 1);
         table.addAttributes("File1.json", "Etiqueta1", "Real Madrid");
         table.addAttributes("File1.json", "Etiqueta2", "Barcelona");
-        table.addAttributes("File1.json", "Etiqueta3", "PSG");
+        table.addAttributes("File1.json", "Etiqueta3", "Manchester City");
 
         List<Number> alturas = new ArrayList<>();
             alturas.add((Number) table.getValue("File1.json", "Valor1"));
@@ -39,7 +41,20 @@ public class TestJson {
             etiquetas.add((String) table.getValue("File1.json", "Etiqueta2"));
             etiquetas.add((String) table.getValue("File1.json", "Etiqueta3"));
 
-        graph(alturas, etiquetas, (String) table.getValue("File1.json", "Titulo"), (String) table.getValue("File1.json", "Eje X"), (String) table.getValue("File1.json", "Eje Y"));
+        barGraph(alturas, etiquetas, (String) table.getValue("File1.json", "Titulo"), (String) table.getValue("File1.json", "Eje X"), (String) table.getValue("File1.json", "Eje Y"));
+
+        List<Number> datos = new ArrayList<>();
+            datos.add((Number) table.getValue("File1.json", "Valor1"));
+            datos.add((Number) table.getValue("File1.json", "Valor2"));
+            datos.add((Number) table.getValue("File1.json", "Valor3"));
+
+        List<String> titulos = new ArrayList<>();
+            titulos.add((String) table.getValue("File1.json", "Etiqueta1"));
+            titulos.add((String) table.getValue("File1.json", "Etiqueta2"));
+            titulos.add((String) table.getValue("File1.json", "Etiqueta3"));
+
+        pieChart(datos, titulos, (String) table.getValue("File1.json", "Titulo1"));
+
 
         /*table.addFile("Project.json");
         table.addAttributes("Project.json", "Titulo", "Grafica de Pie");
@@ -51,7 +66,7 @@ public class TestJson {
         // System.out.println(table.getValue("File2.json", "Valor1"));
     }
 
-    public static void graph(List<Number> alturas, List<String> etiquetas, String titulo, String tituloX, String tituloY) {
+    public static void barGraph(List<Number> alturas, List<String> etiquetas, String titulo, String tituloX, String tituloY) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Gráfica de Barras");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -63,6 +78,19 @@ public class TestJson {
             panel.add(grafica);
 
             frame.add(panel);
+            frame.setVisible(true);
+        });
+    }
+
+    public static void pieChart(List<Number> datos, List<String> titulos, String titulo) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Gráfico de Pie Personalizado");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(600, 400);
+
+            PieChart grafico = new PieChart(datos, titulos, titulo);
+            frame.add(grafico);
+
             frame.setVisible(true);
         });
     }
